cc854a24f2c8c8420ffd872b9f4c733b6f22f6bf
silasrai <silasraii144@gmail.com>
2025-01-25 15:00:15 +0545

    vendor auth

--- config/auth.php
+++ config/auth.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Admin;
+use App\Models\Vendor;
 
 return [
 
@@ -46,6 +47,10 @@ return [
             'driver' => 'session',
             'provider' => 'admins',
         ],
+        'vendor' => [
+            'driver' => 'session',
+            'provider' => 'vendors',
+        ],
     ],
 
     /*
@@ -74,7 +79,11 @@ return [
             'driver' => 'eloquent',
             'model' =>Admin::class
         ],
-
+        
+        'vendors' => [
+            'driver' => 'eloquent',
+            'model' =>Vendor::class
+        ],
         // 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
